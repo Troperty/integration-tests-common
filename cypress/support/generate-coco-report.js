429f4edd61808d095b9e8eefd5de694b6c9c3e31
Michael Mrazek <github@wingnut.se>
2022-03-23 10:19:02 +0100

    Implementing Swagger scraping using Puppeteer in report generation

--- cypress/support/generate-coco-report.js
+++ cypress/support/generate-coco-report.js
@@ -4,22 +4,37 @@ const fetch = require('sync-fetch')
 const RJSON = require("relaxed-json")
 const _ = require('lodash')
 
-exports.generateCoCoReport = function () {
-    const cfgFileName = process.env.COCO_REPORT_CONFIG_FILE ? process.env.COCO_REPORT_CONFIG_FILE : "cypress.json"
-    const cfg = JSON.parse(fs.readFileSync(cfgFileName))
-    console.log("Using cfg:\n" + JSON.stringify(cfg))
+// The two (optional) parameters are the base url (the server to target) and the docs url to Swagger docs
+// It seems to only be needed (so far) for the customer api where we have an /internaldocs suffix rather than the default /docs
+// Default is to use whatever is in the cypress cfg.env.actualHost both as server target and as the swagger root (with suffix /docs)
+exports.generateCoCoReport = async function ({
+    baseUrl = defaultBaseUrl(),
+    docsUrl = defaultDocsUrl()
+} = {}) {
+    
+    console.log("Using baseUrl: " + baseUrl + " and docsUrl: " + docsUrl)
 
-    const targetedEndpoints = extractTargetedEndpointsFromSwagger(cfg)
+    const metadataPaths = await scrapeSwaggerDocsMetadata(docsUrl)
+
+    const targetedEndpoints = await extractTargetedEndpointsFromSwagger(metadataPaths, baseUrl)
     // console.log("\nTargeted endpoints from Swagger:\n" + JSON.stringify(targetedEndpoints))
 
     const calledEndpoints = extractEndpointsCalledDuringSpecRuns()
     // console.log("\nEndpoints called during spec runs:\n" + JSON.stringify(calledEndpoints))
 
     const reportData = createReportData(targetedEndpoints, calledEndpoints)
-    const table = createAsciiTable(reportData, cfg)
+    const table = createAsciiTable(reportData, baseUrl)
 
     fs.writeFileSync("cypress/reports/ascii.txt", table.toString())
     console.log(table.toString())
+}
+
+function defaultBaseUrl() {
+    return JSON.parse(fs.readFileSync("cypress.json")).env.actualHost
+}
+
+function defaultDocsUrl() {
+    return defaultBaseUrl() + "/docs"
 }
 
 function createReportData(targetedEndpoints, calledEndpoints) {
@@ -30,15 +45,11 @@ function createReportData(targetedEndpoints, calledEndpoints) {
     }))
 }
 
-// FIXME Can this be resolved somehow at runtime? Is there a top level endpoint in Swagger we can expose that just lists the others?
-exports.extractTargetedEndpointsFromSwagger = function(cfg) {
+const extractTargetedEndpointsFromSwagger = async function (metadataPaths, hostAndCtxRoot) {
     const targetedEndpoints = []
-    const ctxRoot = new URL(cfg.env.actualHost).pathname
-    // Read the scraped paths from file, strip first part
-    const metadataPaths = fs.readFileSync("cypress/logs/swagger-metadata-paths.json").map(path => path.split("/").slice(1).join("/"))
 
     metadataPaths.forEach(s => {
-        const swaggerUrl = `${cfg.env.actualHost}${s}`
+        const swaggerUrl = `${hostAndCtxRoot}/${s}`
         const swaggerMetadataJson = fetch(swaggerUrl, { /* empty options */ }).json()
 
         for (const [path, value] of Object.entries(swaggerMetadataJson.paths)) {
@@ -46,13 +57,32 @@ exports.extractTargetedEndpointsFromSwagger = function(cfg) {
             const httpVerb = Object.entries(value)[0][0].toUpperCase()
             const nodeStylePath = convertPathParams(path)
             // path is the last part of the url from (typically what comes after <protocol>://<host>:<port>/webapi/)
-            targetedEndpoints.push({ method: httpVerb, path: ctxRoot + nodeStylePath })
+            targetedEndpoints.push({ method: httpVerb, path: new URL(hostAndCtxRoot).pathname + nodeStylePath })
         }
     })
 
     // Since we sometimes have overlapping tags in Swagger, we do this just to not get duplicate paths
     const uniqueTargetedEndpoints = _.uniqWith(targetedEndpoints, _.isEqual)
+    // console.log(uniqueTargetedEndpoints)
     return uniqueTargetedEndpoints
+}
+
+const scrapeSwaggerDocsMetadata = async function (swaggerDocsUrl) {
+    const puppeteer = require('puppeteer')
+    const browser = await puppeteer.launch({})
+
+    const page = await browser.newPage()
+    await page.goto(swaggerDocsUrl)
+
+    const options = await page.$x("//select/option")
+    // Extract the values of the swagger doc dropdown (holds the paths to each respective service Swagger doc page)
+    const optionTexts = await Promise.all(options.map(async (option) =>
+        await page.evaluate(el => el.value.split("/").slice(2).join("/"), option)
+    ))
+
+    browser.close()
+
+    return optionTexts
 }
 
 function extractEndpointsCalledDuringSpecRuns() {
@@ -61,7 +91,7 @@ function extractEndpointsCalledDuringSpecRuns() {
     // Skip any other lines, we're only interested in the outgoing calls made during the spec runs
     const requests = fs.readFileSync("cypress/logs/request.log").toString().split("\n")
         .filter(s => s.includes("eventName: 'http:request'"))
-    console.log("### Found " + requests.length + " outgoing calls in request log")
+    console.log("Found " + requests.length + " outgoing calls in request log")
 
     // JSON part is after the log ceremony: 2022-03-17T07:56:06.697Z cypress:server:socket-base backend:request { THIS IS THE JSON PART WE'RE AFTER }
     const requestsAsJson = requests.map(r => {
@@ -81,21 +111,22 @@ function extractEndpointsCalledDuringSpecRuns() {
     return calledEndpoints;
 }
 
-function createAsciiTable(reportData, cfg) {    
+function createAsciiTable(reportData, hostAndCtxRoot) {
     const { AsciiTable3, AlignmentEnum } = require('ascii-table3')
 
     const data = []
     reportData.forEach(e => data.push([e.called ? "\u2713" : '', e.method, e.path]))
 
+    const url = new URL(hostAndCtxRoot)
     const table =
-        new AsciiTable3('Code Coverage Report - ' + cfg.env.actualHost)
+        new AsciiTable3('Code Coverage Report - ' + url.protocol + "//" + url.hostname)
             .setHeading('Called', 'Method', "Path")
             .addRowMatrix(data);
 
     table.setStyle('unicode-double')
-    .setAlign(1, AlignmentEnum.CENTER)
-    .setAlign(2, AlignmentEnum.CENTER)
-    .setAlign(3, AlignmentEnum.LEFT)
+        .setAlign(1, AlignmentEnum.CENTER)
+        .setAlign(2, AlignmentEnum.CENTER)
+        .setAlign(3, AlignmentEnum.LEFT)
     return table
 }
 
